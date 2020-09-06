35b6cc037748a667d5b92d15126b688e5813a055
Manali Sharma <manali.sharma_cs17@gla.ac.in>
2020-09-06 19:51:08 +0530

    Update Bubble_Sort.java

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -23,35 +23,13 @@ public class Main
 	}
 }
 
-
-
-import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    int n=sc.nextInt();
-	    int a[]=new int[n];
-	    int c=0;
-	    for(int i=0;i<n;i++)
-	    a[i]=sc.nextInt();
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<n-i-1;j++){
-	            if(a[j]>a[j+1]){
-	                int temp =a[j];
-	                a[j]=a[j+1];
-	                a[j+1]=temp;
-	            }
-	        }
-	    }
-	    for(int i=0;i<n;i++)
-		System.out.print(a[i]+" ");
-		
-	}
-}
-
 i/p:
 5                                                                                                                             
-5 1 4 2 8 
+5 1 4 2 8
+	
 o/p:                                                                                                                    
 1 2 4 5 8 
+	
+Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+
+Best Case Time Complexity: O(n). Best case occurs when array is already sorted.	
